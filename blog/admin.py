e1bb1adc06722ccd8086b34b4b9c744d0627cfed
Jeffrey <okoobohefosa@gmail.com>
2020-02-25 23:17:18 +0100

    added the ArticleAdmin class.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
 from .models import Article
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'date', 'writer')
+
+
+admin.site.register(Article, ArticleAdmin)
