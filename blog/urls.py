a3288e1256ab2246d1b2ab7f035c0bf94c7c07a0
Jeffrey <okoobohefosa@gmail.com>
2020-04-06 17:25:17 +0100

    fix articles issue, add / to route name and add delete route to remove post

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,10 @@ from blog import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('about', views.AboutPage.as_view(), name='about'),
+    path('about/', views.AboutPage.as_view(), name='about'),
     path('<slug:slug>', views.DetailsPage.as_view(), name='details'),
-    path('articles', views.ArticlesPage.as_view(), name='articles'),
+    path('articles/', views.ArticlesPage.as_view(), name='articles'),
     path('new/', views.CreatePost.as_view(), name='create_post'),
-    path('<slug:slug>/update', views.UpdatePost.as_view(), name='update_post')
+    path('<slug:slug>/update', views.UpdatePost.as_view(), name='update_post'),
+    path('<slug:slug>/delete', views.DeletePost.as_view(), name='delete_post')
 ]
