8a29ea6f8d1d2e7d982d2bb1dbdb81b00859ef8d
Jeffrey <okoobohefosa@gmail.com>
2020-05-19 18:12:18 +0100

    revamp screen margin

--- users/forms.py
+++ users/forms.py
@@ -5,22 +5,22 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserForm(UserCreationForm):
     first_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={
-        'class': 'input',
+        'class': 'input100',
     }))
     last_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={
-        'class': 'input'
+        'class': 'input100'
     }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'input'
+        'class': 'input100'
     }))
     username = forms.CharField(max_length=40, widget=forms.TextInput(attrs={
-        'class': 'input'
+        'class': 'input100'
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'input'
+        'class': 'input100'
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'input'
+        'class': 'input100'
     }))
 
     class Meta:
